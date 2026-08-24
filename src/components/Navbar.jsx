5479fc9503cf41d66d29c7cac808e674442e12d9
surayaafroze63 <surayaafroze63@gmail.com>
2026-08-24 14:15:00 +0600

    feat: update Navbar destinations link to add_destinations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className='flex justify-between w-7xl mx-auto p-5'>
         <ul className='flex justify-between  gap-4 '>
           <li><Link href='/'>Home</Link></li>
-          <li><Link href='/destinations'>Destinations</Link></li>
+          <li><Link href='/add_destinations'>Add-Destinations</Link></li>
           <li><Link href='/my_bookings'>My Bookings</Link></li>
           <li><Link href='/admin'>Admin</Link></li>
         </ul>
