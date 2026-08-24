6737a93a21ce78fa23135bcbb15572aaef279b22
surayaafroze63 <surayaafroze63@gmail.com>
2026-08-24 14:00:00 +0600

    refactor: move Footer from Home page to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar></Navbar>
-        {children}</body>
+        {children}
+          <Footer></Footer>
+        </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Banner from "@/components/Banner";
-import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
     <div>
      <Banner></Banner>
-   <Footer></Footer>
+ 
    </div>
   );
 }
